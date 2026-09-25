6966b512602b3bb571ee56a755d256287219d129
Rrachet <136880232+Rrachet@users.noreply.github.com>
2026-09-25 17:36:55 +0530

    feat: establish ClinicIT Spring Boot foundation

---
+++ src/main/java/com/clinicit/ClinicITApplication.java
@@ -0,0 +1,12 @@
+package com.clinicit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicITApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicITApplication.class, args);
+    }
+}
